bf5e863c67394068b03ea2a5e127849133f740c5
Anders Søgaard <andershagbard@gmail.com>
2021-10-20 15:33:58 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+};
