80470cec01f93a8b68d36853e32bf56f80e7f165
andershagbard <andershagbard@gmail.com>
2023-02-22 15:09:03 +0100

    Add todo

--- type/json/ProductImage.ts
+++ type/json/ProductImage.ts
@@ -3,7 +3,7 @@ import Image from './Image';
 export default interface ProductImage extends Image {
   product_id: number
   position: number
-  updated_at: string
+  updated_at: string // TODO: Check if updated at should be in "Image"
   width: number
   height: number
   variant_ids: number[]
