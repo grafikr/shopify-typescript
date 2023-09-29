2a564941cf3fbd266ecf536900449be99764d1a6
Anders Søgaard <9662430+andershagbard@users.noreply.github.com>
2023-09-29 09:46:21 +0200

    Update customerPrivacy.ts

--- global/modules/customerPrivacy.ts
+++ global/modules/customerPrivacy.ts
@@ -1,15 +1,44 @@
+type YesNo = '' | 'yes' | 'no';
+
 export default interface CustomerPrivacy {
-  getCCPAConsent(): string
-  getRegulation(): string
-  getShopPrefs(): {
-    limit: string[]
-  }
-  getTrackingConsent(): string
-  isRegulationEnforced(): boolean
-  setCCPAConsent(consented: boolean, callback: CallableFunction): void
-  setTrackingConsent(consented: boolean, callback: CallableFunction): void
-  shouldShowCCPABanner(): boolean
-  shouldShowGDPRBanner(): boolean
-  userCanBeTracked(): boolean
-  userDataCanBeSold(): boolean
+  analyticsProcessingAllowed(): boolean;
+  currentVisitorConsent(): {
+    marketing: YesNo;
+    analytics: YesNo;
+    preferences: YesNo;
+    sale_of_data: YesNo;
+    gpc: YesNo;
+  };
+  doesMerchantSupportGranularConsent(): boolean;
+  firstPartyMarketingAllowed(): boolean;
+  getCCPAConsent(): YesNo;
+  getRegulation(): string;
+  getShopPrefs(): { limit: Array<string> };
+  getTrackingConsent(): YesNo;
+  isRegulationEnforced(): boolean;
+  preferencesProcessingAllowed(): boolean;
+  saleOfDataRegion(): boolean;
+  setCCPAConsent(consent: boolean, callback: () => void): void;
+  setTrackingConsent(
+    consent: {
+          analytics?: boolean;
+          marketing?: boolean;
+          preferences?: boolean;
+          sale_of_data?: boolean;
+        },
+    callback: () => void,
+  ): void;
+  shouldShowBanner(): boolean;
+  thirdPartyMarketingAllowed(): boolean;
+
+  /** @deprecated Use consent as an object instead */
+  setTrackingConsent(consent: boolean, callback: () => void): void;
+  /** @deprecated Use saleOfDataRegion instead */
+  shouldShowCCPABanner(): boolean;
+  /** @deprecated Use shouldShowBanner instead */
+  shouldShowGDPRBanner(): boolean;
+  /** @deprecated */
+  userCanBeTracked(): boolean;
+  /** @deprecated */
+  userDataCanBeSold(): boolean;
 }
