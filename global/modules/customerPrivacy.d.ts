cad6cd346b67857c9aca24fa5e76560fbae3be39
andershagbard <andershagbard@gmail.com>
2025-09-18 11:24:43 +0200

    Add new privacy features

--- global/modules/customerPrivacy.d.ts
+++ global/modules/customerPrivacy.d.ts
@@ -2,6 +2,7 @@ type YesNo = '' | 'yes' | 'no';
 
 export type CustomerPrivacy = {
   analyticsProcessingAllowed(): boolean;
+  consentId(): string;
   currentVisitorConsent(): {
     marketing: YesNo;
     analytics: YesNo;
@@ -12,6 +13,8 @@ export type CustomerPrivacy = {
   doesMerchantSupportGranularConsent(): boolean;
   firstPartyMarketingAllowed(): boolean;
   getCCPAConsent(): YesNo;
+  /** Returns a ISO 3166-2-formatted string with a two character country code, followed by an optional one to three character region code. Returns an empty string if the location can't be determined. */
+  getRegion(): string;
   getRegulation(): string;
   getShopPrefs(): { limit: Array<string> };
   getTrackingConsent(): YesNo;
